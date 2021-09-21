6723504d39220db9d4737194d173b04b6814b3db
SaschaS93 <43013380+SaschaS93@users.noreply.github.com>
2021-09-21 20:42:48 +0200

    Add first protocol implementations and constants

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello");
+import { TelnetMessage, Tn5250Message } from "./protocol.js";
+
+console.log(TelnetMessage.TYPE);
+console.log(Tn5250Message.TERMINAL.IBM31792);
+console.log(Tn5250Message.COMMAND.TEST == 0xff);
+new TelnetMessage();
---
+++ protocol.js
@@ -0,0 +1,145 @@
+"use strict";
+
+class Protocol {
+
+    constructor() {
+        if (new.target === Protocol) {
+          throw new TypeError("Cannot construct Protocol instances directly");
+        }
+        console.log("hello");
+    }
+
+    serialize() {
+        console.log("serialize() not implemented");
+    }
+
+    deserialize() {
+        console.log("deserialize() not implemented");
+    }
+
+}
+
+// https://datatracker.ietf.org/doc/html/rfc854
+// https://www.ibm.com/docs/en/zos/2.3.0?topic=problems-telnet-commands-options
+// https://datatracker.ietf.org/doc/html/rfc1060
+export class TelnetMessage extends Protocol {
+
+    /**
+     * Return Telnet commands from RFC 854
+     */
+    static get COMMAND() { 
+        return {
+
+            // End of subnegotiation parameters.
+            SE_END_OF_SUBNEGOTIATION_PARAMETERS: 0xf0,
+
+            // No operation.
+            NOP_NO_OPERATION: 0xf1,
+
+            // The data stream portion of a Synch. This should always be accompanied by a TCP Urgent notification.
+            DATA_MARK: 0xf2,
+
+            // NVT character BRK.
+            BRK_BREAK: 0xf3,
+
+            // The function IP.
+            IP_INTERRUPT_PROCESS: 0xf4,
+
+            // The function AO.
+            AO_ABORT_OUTPUT: 0xf5,
+
+            // The function AYT.
+            AYT_ARE_YOU_THERE: 0xf6,
+
+            // The function EC.
+            EC_ERASE_CHARACTER: 0xf7,
+
+            // The function EL.
+            EL_ERASE_LINE: 0xf8,
+
+            // The GA signal.
+            GA_GO_AHEAD: 0xf9,
+
+            // Indicates that what follows is subnegotiation of the indicated option.
+            SB_SUBNEGOTIATION: 0xfa,
+
+            // Indicates the want to begin performing, or confirmation that you are now performing, the indicated option.
+            WILL: 0xfb,
+
+            // Indicates the refusal to perform, or continue performing, the indicated option.
+            WONT: 0xfc,
+
+            // Indicates the request that the other party perform, or confirmation that you are expecting the other party to perform, the indicated option.
+            DO: 0xfd,
+
+            // Indicates the demand that the other party stop performing, or confirmation that you are no longer expecting the other party to perform, the indicated option.
+            DONT: 0xfe,
+
+            // Data byte 255.
+            IAC_INTERPRET_AS_COMMAND: 0xff
+        }
+    }
+
+    /**
+     * Return Telnet subcommands from RFC 1060
+     */
+    static get COMMAND_OPTION() {
+        return {
+            BINARY_TRANSMISSION: 0x00,
+            ECHO: 0x01,
+            RECONNECTION: 0x02,
+            SUPPRESS_GO_AHEAD: 0x03,
+            APPROX_MESSAGE_SIZE_NEGOTIATION: 0x04,
+            STATUS: 0x05,
+            TIMING_MARK: 0x06,
+            REMOTE_CONTROLLED_TRANS_AND_ECHO: 0x07,
+            OUTPUT_LINE_WIDTH: 0x08,
+            OUTPUT_PAGE_SIZE: 0x09,
+            OUTPUT_CARRIAGE_RETURN_DISPOSITION: 0x0a,
+            OUTPUT_HORIZONTAL_TAB_STOPS: 0x0b,
+            OUTPUT_HORIZONTAL_TAB_DISPOSITION: 0x0c,
+            OUTPUT_FORMFEED_DISPOSITION: 0x0d,
+            OUTPUT_VERTICAL_TABSTOPS: 0x0e,
+            OUTPUT_VERTICAL_TAB_DISPOSITION: 0x0f,
+            OUTPUT_LINEFEED_DISPOSITION: 0x10,
+            OUTPUT_EXTENDED_ASCII: 0x11,
+            LOGOUT: 0x12,
+            BYTE_MACRO: 0x13,
+            DATA_ENTRY_TERMINAL: 0x14,
+            SUPDUP: 0x15,
+            SUPDUP_OUTPUT: 0x16,
+            SEND_LOCATION: 0x17,
+            TERMINAL_TYPE: 0x18,
+            END_OF_RECORD: 0x19,
+            TACACS_USER_IDENTIFICATION: 0x1a,
+            OUTPUT_MARKING: 0x1b,
+            TERMINAL_LOCATION_NUMBER: 0x1c,
+            TELNET_3270_REGIME: 0x1d,
+            X3_PAD: 0x1e,
+            NEGOTIATE_ABOUT_WINDOW_SIZE: 0x1f,
+            TERMINAL_SPEED: 0x20,
+            REMOTE_FLOW_CONTROL: 0x21,
+            LINEMODE: 0x22,
+            X_DISPLAY_LOCATION: 0x23,
+            EXTENDED_OPTIONS_LIST: 0xff
+        }
+    }
+}
+
+// https://datatracker.ietf.org/doc/html/rfc1205
+export class Tn5250Message extends TelnetMessage {
+    static get TERMINAL() {
+        return {
+            IBM5555C01: "IBM-5555-C01", // 24 x 80 Double-Byte Character Set color display
+            IBM5555B01: "IBM-5555-B01", // 24 x 80 Double-Byte Character Set (DBCS)
+            IBM3477FC:  "IBM-3477-FC",  // 27 x 132 color display
+            IBM3477FG:  "IBM-3477-FG",  // 27 x 132 monochrome display
+            IBM31802:   "IBM-3180-2",   // 27 x 132 monochrome display
+            IBM31792:   "IBM-3179-2",   // 24 x 80 color display
+            IBM3196A1:  "IBM-3196-A1",  // 24 x 80 monochrome display
+            IBM52922:   "IBM-5292-2",   // 24 x 80 color display
+            IBM52911:   "IBM-5291-1",   // 24 x 80 monochrome display
+            IBM525111:  "IBM-5251-11"   // 24 x 80 monochrome display
+        } 
+    }
+}
